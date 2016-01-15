2755e9d2150c1d9237af0e92137280c2fd2f1497
evanchawu <r00943150@ntu.edu.tw>
2016-01-15 23:10:15 +0800

    Drop guys table. ( guy table just for testing)
    Guys table just for testing,
    Don't know why there is no Guy model and create_guy migration file but guy table still exists in schema ?
    so create drop_guys migration to delete it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115025351) do
+ActiveRecord::Schema.define(version: 20160115150335) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "zombie_id"
